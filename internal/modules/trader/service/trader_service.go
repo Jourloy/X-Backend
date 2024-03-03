c222886265d60b160e45751e3901ba995fd2bf94
Jourloy <Jourloy@yandex.ru>
2024-03-03 17:11:53 +0300

    🔧 Fix trader

---
+++ internal/modules/trader/service/trader_service.go
@@ -0,0 +1,123 @@
+package trader_service
+
+import (
+	"errors"
+	"os"
+
+	"github.com/charmbracelet/log"
+	"github.com/redis/go-redis/v9"
+
+	"github.com/jourloy/X-Backend/internal/cache"
+	"github.com/jourloy/X-Backend/internal/repositories"
+	"github.com/jourloy/X-Backend/internal/repositories/account_rep"
+	"github.com/jourloy/X-Backend/internal/repositories/sector_rep"
+	"github.com/jourloy/X-Backend/internal/repositories/trader_rep"
+)
+
+var (
+	logger = log.NewWithOptions(os.Stderr, log.Options{
+		Prefix: `[trader]`,
+		Level:  log.DebugLevel,
+	})
+)
+
+type Service struct {
+	traRep repositories.ITraderRepository
+	secRep repositories.ISectorRepository
+	accRep repositories.IAccountRepository
+	cache  redis.Client
+}
+
+// Init создает сервис торговца
+func Init() *Service {
+
+	traRep := trader_rep.Repository
+	secRep := sector_rep.Repository
+	accRep := account_rep.Repository
+
+	logger.Info(`Service initialized`)
+
+	return &Service{
+		traRep: traRep,
+		secRep: secRep,
+		accRep: accRep,
+		cache:  *cache.Client,
+	}
+}
+
+type createResp struct {
+	Err error
+}
+
+// Create создает торговца
+func (s *Service) Create(body repositories.Trader, accountID string) createResp {
+	// Проверка существования аккаунта
+	account := s.accRep.GetOne(accountID)
+	if account.ID == `` {
+		return createResp{Err: errors.New(`account not found`)}
+	}
+
+	// Проверка существования сектора
+	sector := s.secRep.GetOne(body.SectorID)
+	if sector.ID == `` {
+		return createResp{Err: errors.New(`sector not found`)}
+	}
+
+	s.traRep.Create(&body, accountID)
+	return createResp{Err: nil}
+}
+
+type getOneResp struct {
+	Err    error
+	Trader repositories.Trader
+}
+
+// GetOne получает торговца по его ID
+func (s *Service) GetOne(id string, accountID string) {
+	s.traRep.GetOne(id, accountID)
+}
+
+type getAllResp struct {
+	Err     error
+	Traders []repositories.Trader
+}
+
+// GetAll возвращает всех торговцев
+func (s *Service) GetAll(query repositories.TraderGetAll, accountID string) getAllResp {
+	// Получение работников
+	traders := s.traRep.GetAll(query, accountID)
+	return getAllResp{
+		Err:     nil,
+		Traders: traders,
+	}
+}
+
+type updateOneResp struct {
+	Err error
+}
+
+// UpdateOne обновляет торговца
+func (s *Service) UpdateOne(body repositories.Trader, accountID string) updateOneResp {
+	// Перезапись accountID для безопасности
+	body.AccountID = accountID
+
+	s.traRep.UpdateOne(&body)
+	return updateOneResp{
+		Err: nil,
+	}
+}
+
+type deleteOneResp struct {
+	Err error
+}
+
+// DeleteOne удаляет торговца
+func (s *Service) DeleteOne(body repositories.Trader, accountID string) deleteOneResp {
+	// Перезапись accountID для безопасности
+	body.AccountID = accountID
+
+	s.traRep.DeleteOne(&body)
+	return deleteOneResp{
+		Err: nil,
+	}
+}
--- internal/modules/trader/trader.go
+++ internal/modules/trader/trader.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
 
+	trader_service "github.com/jourloy/X-Backend/internal/modules/trader/service"
 	"github.com/jourloy/X-Backend/internal/repositories"
 	"github.com/jourloy/X-Backend/internal/tools"
 )
@@ -19,26 +20,23 @@ var (
 	})
 )
 
-type TraderService struct {
-	wRep  repositories.ITraderRepository
-	cRep  repositories.IVillageRepository
-	cache redis.Client
+type Controller struct {
+	service trader_service.Service
 }
 
-// InitTraderService создает сервис торговца
-func InitTraderService(wRep repositories.ITraderRepository, cRep repositories.IVillageRepository, cache redis.Client) *TraderService {
+// Init создает сервис торговца
+func Init() *Controller {
 
-	logger.Info(`TraderService initialized`)
+	service := trader_service.Init()
+	logger.Info(`Controller initialized`)
 
-	return &TraderService{
-		wRep:  wRep,
-		cRep:  cRep,
-		cache: cache,
+	return &Controller{
+		service: *service,
 	}
 }
 
 // Create создает торговца
-func (s *TraderService) Create(c *gin.Context) {
+func (s *Controller) Create(c *gin.Context) {
 	accountID := c.GetString(`accountID`)
 
 	// Парсинг body
@@ -48,32 +46,37 @@ func (s *TraderService) Create(c *gin.Context) {
 		c.JSON(400, gin.H{`error`: `Parse body error`})
 	}
 
-	// Проверка существования поселения
-	villageID := c.Query(`villageID`)
-	if villageID == `` {
-		logger.Error(`villageID is required`)
-		c.JSON(400, gin.H{`error`: `villageID is required`})
+	resp := s.service.Create(body, accountID)
+	if resp.Err != nil {
+		logger.Error(resp.Err)
+		c.JSON(400, gin.H{`error`: resp.Err.Error()})
 	}
 
-	village := s.cRep.GetOne(villageID, accountID)
-	if village.ID == `` {
-		logger.Error(`Village not found`)
-		c.JSON(404, gin.H{`error`: `Village not found`})
-	}
-
-	s.wRep.Create(&body, villageID, accountID)
 	c.JSON(200, gin.H{`error`: ``})
 }
 
 // GetOne получает торговца по его ID
-func (s *TraderService) GetOne(c *gin.Context) {
+func (s *Controller) GetOne(c *gin.Context) {
 	accountID := c.GetString(`accountID`)
 
-	s.wRep.GetOne(c.Query(`id`), accountID)
+	// Получение ID рабочего
+	traderID := c.Query(`traderID`)
+	if traderID == `` {
+		logger.Error(`traderID is required`)
+		c.JSON(400, gin.H{`error`: `traderID is required`})
+	}
+
+	resp := s.service.GetOne(traderID, accountID)
+	if resp.Err != nil {
+		logger.Error(resp.Err)
+		c.JSON(400, gin.H{`error`: resp.Err.Error()})
+	}
+
+	c.JSON(200, gin.H{`error`: ``, `worker`: resp.Worker})
 }
 
 // GetAll возвращает всех торговцев
-func (s *TraderService) GetAll(c *gin.Context) {
+func (s *Controller) GetAll(c *gin.Context) {
 	accountID := c.GetString(`accountID`)
 
 	// Создание фильтров
@@ -113,7 +116,7 @@ func (s *TraderService) GetAll(c *gin.Context) {
 }
 
 // UpdateOne обновляет торговца
-func (s *TraderService) UpdateOne(c *gin.Context) {
+func (s *Controller) UpdateOne(c *gin.Context) {
 	accountID := c.GetString(`accountID`)
 
 	// Парсинг body
@@ -131,7 +134,7 @@ func (s *TraderService) UpdateOne(c *gin.Context) {
 }
 
 // DeleteOne удаляет торговца
-func (s *TraderService) DeleteOne(c *gin.Context) {
+func (s *Controller) DeleteOne(c *gin.Context) {
 	accountID := c.GetString(`accountID`)
 
 	// Парсинг body
